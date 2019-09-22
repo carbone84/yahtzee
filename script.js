ea732273e2ed2e12547f1562f0cc93e41aa178e0
Bryan <bryanacarbone@gmail.com>
2019-09-22 15:22:21 -0400

    roll, new game buttons working

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-var roll_button = document.querySelector("#roll");
+var rollsLeft = 3;
+var rollsLeft_span = document.querySelector(".rollsLeft");
+var roll_button = document.querySelector("#rollButton");
+var newGame_button = document.querySelector("#newGame");
 var diceArr_div = document.querySelectorAll(".dice ul");
 
-roll_button.addEventListener("click", function() {
-    rollDice();
+newGame_button.addEventListener("click", function() {
+    rollsLeft = 3;
+    rollsLeft_span.innerHTML = rollsLeft;
+    for(var i = 0; i < diceArr_div.length; i++) {
+        diceArr_div[i].innerHTML = getDiceLis(i + 1);
+    }
 })
+
+roll_button.addEventListener("click", function() {
+    clearDice();
+    rollDice();
+    rollsLeft_span.innerHTML = rollsLeft;
+})
+
+function clearDice() {
+
+    for(var i = 0; i < diceArr_div.length; i++) {
+        diceArr_div[i].innerHTML = getDiceLis(0);
+    }
+}
 
 function rollDice() {
     for(var i = 0; i < diceArr_div.length; i++) {
         diceArr_div[i].innerHTML = getDiceLis(Math.ceil(Math.random()*5));
     }
+    rollsLeft--;
 }
 
 function getDiceLis(num) {
@@ -23,5 +44,7 @@ function getDiceLis(num) {
             return "<li class=\"top-left\"></li><li class=\"bot-left\"></li><li class=\"top-right\"></li><li class=\"bot-right\"></li>";
         case 5:
             return "<li class=\"top-left\"></li><li class=\"bot-left\"></li><li class=\"center\"></li><li class=\"top-right\"></li><li class=\"bot-right\"></li>";
+        case 0:
+            return "";
     }
 }
