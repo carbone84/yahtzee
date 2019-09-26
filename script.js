e01842899c0eb982047c80628624dab19de89d4d
Bryan <bryanacarbone@gmail.com>
2019-09-25 20:26:54 -0400

    working roll and new game buttons, dice selection

--- script.js
+++ script.js
@@ -2,34 +2,59 @@ var rollsLeft = 3;
 var rollsLeft_span = document.querySelector(".rollsLeft");
 var roll_button = document.querySelector("#rollButton");
 var newGame_button = document.querySelector("#newGame");
-var diceArr_div = document.querySelectorAll(".dice ul");
+var diceArr_ul = document.querySelectorAll(".dice ul");
+var diceArr_div = document.querySelectorAll(".dice");
+var scoreArr_td = document.querySelectorAll(".score");
 
-newGame_button.addEventListener("click", function() {
-    rollsLeft = 3;
-    rollsLeft_span.innerHTML = rollsLeft;
-    for(var i = 0; i < diceArr_div.length; i++) {
-        diceArr_div[i].innerHTML = getDiceLis(i + 1);
+init();
+
+function init() {
+    for(var i = 0; i < diceArr_ul.length; i++) {
+        diceArr_div[i].addEventListener("click", function() {
+            this.classList.toggle("selected");
+        });
     }
-})
 
-roll_button.addEventListener("click", function() {
-    clearDice();
-    rollDice();
-    rollsLeft_span.innerHTML = rollsLeft;
-})
-
-function clearDice() {
-
-    for(var i = 0; i < diceArr_div.length; i++) {
-        diceArr_div[i].innerHTML = getDiceLis(0);
+    for(var i = 0; i < scoreArr_td.length; i++) {
+        scoreArr_td[i].addEventListener("click", function() {
+            alert("Hello");
+        })
     }
 }
 
-function rollDice() {
-    for(var i = 0; i < diceArr_div.length; i++) {
-        diceArr_div[i].innerHTML = getDiceLis(Math.ceil(Math.random()*5));
+newGame_button.addEventListener("click", function() {
+    resetGame();
+})
+
+function resetGame() {
+    rollsLeft = 3;
+    rollsLeft_span.innerHTML = rollsLeft;
+    for(var i = 0; i < diceArr_ul.length; i++) {
+        diceArr_ul[i].innerHTML = getDiceLis(i + 1);
+        diceArr_div[i].classList.remove("selected");
     }
-    rollsLeft--;
+}
+
+roll_button.addEventListener("click", function() {
+    if(rollsLeft > 0) {
+        for(var i = 0; i < diceArr_ul.length; i++) {
+            if (!diceArr_ul[i].parentElement.classList.contains("selected")) {
+                rollDice(diceArr_ul[i]);
+            }
+        }
+        rollsLeft--;
+        rollsLeft_span.innerHTML = rollsLeft;
+    }
+})
+
+function clearDice() {
+    for(var i = 0; i < diceArr_ul.length; i++) {
+        diceArr_ul[i].innerHTML = "";
+    }
+}
+
+function rollDice(dice) {
+    dice.innerHTML = getDiceLis(Math.ceil(Math.random() * 5));
 }
 
 function getDiceLis(num) {
@@ -44,7 +69,7 @@ function getDiceLis(num) {
             return "<li class=\"top-left\"></li><li class=\"bot-left\"></li><li class=\"top-right\"></li><li class=\"bot-right\"></li>";
         case 5:
             return "<li class=\"top-left\"></li><li class=\"bot-left\"></li><li class=\"center\"></li><li class=\"top-right\"></li><li class=\"bot-right\"></li>";
-        case 0:
-            return "";
+        // case 0:
+        //     return "";
     }
 }
